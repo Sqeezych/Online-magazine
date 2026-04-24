c3d145d374d6d6731480cb43cc9db0b654d2ed83
Kirill <sqeezy98@gmail.com>
2026-04-24 10:43:16 +0300

    Initial Typescript

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,17 @@ import { BrowserRouter } from 'react-router-dom';
 import { Magazine } from './magazine.jsx';
 import { Provider } from 'react-redux';
 import { store } from './store.js';
+// @ts-ignore
 import './index.css';
 
-createRoot(document.getElementById('root')).render(
+const rootElement = document.getElementById('root');
+if (!rootElement) {
+  throw new Error('Root element is not found');
+}
+
+const root = createRoot(rootElement);
+
+root.render(
 	<BrowserRouter>
 		<Provider store={store}>
 			<Magazine />
